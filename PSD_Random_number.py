69969443f38f4185e771a0624b22e9e2bd67c4b2
Casper Strand <casper.strand@stud.noroff.no>
2021-11-12 18:52:38 +0100

    Added pre- and postconditions

--- PSD_Random_number.py
+++ PSD_Random_number.py
@@ -1,7 +1,9 @@
 import random
 class rannumber:
 
-
+    #The init checks the parameteres and sees if they are
+    #numbers and positive. If these two pre conditions are met
+    #an instance of the class is created. 
     def __init__(self, amount, minRange, maxRange):
         check = [amount, minRange, maxRange]
         if all([type(x) == int and x >= 0 for x in check]):
@@ -11,9 +13,10 @@ class rannumber:
         else:
             print("Input needs to be a positive number")
 
+    #The function return the amount of number based on its self.amount
+    #value and the range for the random numbers are set with
+    #self.minRange and self.maxRange. 
     def getRandomNumber(self):
         for x in range(self.amount):
             print(random.randint(self.minRange, self.maxRange))
 
-test = rannumber(3,4,8)
-test.getRandomNumber()
